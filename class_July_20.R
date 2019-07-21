9a2cbbe64a211bd4dd7f8daee71a9d408da8043c
CWWhitney <whitney.cory@gmail.com>
2019-07-21 08:22:56 +0800

    more scripts added

--- class_July_20.R
+++ class_July_20.R
@@ -101,5 +101,204 @@ Radialplot_skinnyblue <- function(data, analysis) {
 Radial_plot(ethnobotanydata, URs)
 Radialplot_skinnyblue(ethnobotanydata, URs)
 
+#Class July 20 2019
 
+#Make function red####
+#copy and paste after calling 'ethnoChord' in the Console
+New_Chord_function_red<- function(data) { #use the '<-' to assign function to new object
+  if (!requireNamespace("reshape", quietly = TRUE)) {
+    stop("Package \"reshape\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+  if (!requireNamespace("circlize", quietly = TRUE)) {
+    stop("Package \"circlize\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+  if (!requireNamespace("dplyr", quietly = TRUE)) {
+    stop("Package \"dplyr\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+  if (!requireNamespace("magrittr", quietly = TRUE)) {
+    stop("Package \"magrittr\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+  
+  sp_name <- informant <- value <- strwidth <- NULL # Setting the variables to NULL first, appeasing R CMD check
+  
+  #add error stops with validate_that
+  assertthat::validate_that("informant" %in% colnames(data), msg = "The required column called \"informant\" is missing from your data. Add it.")
+  assertthat::validate_that("sp_name" %in% colnames(data), msg = "The required column called \"sp_name\" is missing from your data. Add it.")
+  
+  assertthat::validate_that(is.factor(data$informant), msg = "The \"informant\" is not a factor variable. Transform it.")
+  assertthat::validate_that(is.factor(data$sp_name), msg = "The \"sp_name\" is not a factor variable. Transform it.")
+  
+  assertthat::validate_that(all(sum(dplyr::select(data, -informant, -sp_name)>0)) , msg = "The sum of all UR is not greater than zero. Perhaps not all uses have values or are not numeric.")
+  
+  ## Use 'complete.cases' from stats to get to the collection of obs without NA
+  data_complete<-data[stats::complete.cases(data), ]
+  #message about complete cases
+  assertthat::see_if(length(data_complete) == length(data), msg = "Some of your observations included \"NA\" and were removed. Consider using \"0\" instead.")
+  
+  #Melt ethnobotany data
+  mat <- reshape::melt(data, id=c("informant","sp_name")) %>% dplyr::filter(value >=1)%>%
+    dplyr::arrange(dplyr::desc(informant)) %>%  dplyr::arrange(dplyr::desc(sp_name)) %>% dplyr::select(2:3)
+  
+  #Create chord plot
+  
+  circlize::chordDiagram(mat, annotationTrack = "grid", 
+                         preAllocateTracks = list(track.height = max(graphics::strwidth(unlist(dimnames(mat))))))
+  circlize::circos.track(track.index = 1, panel.fun = function(x, y) {
+    xlim = circlize::get.cell.meta.data("xlim")
+    xplot = circlize::get.cell.meta.data("xplot")
+    ylim = circlize::get.cell.meta.data("ylim")
+    sector.name = circlize::get.cell.meta.data("sector.index")
+    
+    if(abs(xplot[2] - xplot[1]) < 20) {
+      circlize::circos.text(mean(xlim), ylim[1], sector.name, facing = "clockwise",
+                            niceFacing = TRUE, adj = c(0, 0.5), col = "red")
+    } else {
+      circlize::circos.text(mean(xlim), ylim[1], sector.name, facing = "clockwise",
+                            niceFacing = TRUE, adj = c(0, 0.5), col = "red")
+    }
+  }, bg.border = NA)
+  
+  print("Chord diagram for each use related to each species in the data set")
+}
+
+#Running new red function
+New_Chord_function_red(ethnobotanydata)
+
+#Copy paste from R example ####
+
+#Use built-in ethnobotany data example
+ethnoChord(ethnobotanydata)
+
+#Generate random dataset of three informants uses for four species
+eb_data <- data.frame(replicate(10,sample(0:1,20,rep=TRUE)))
+names(eb_data) <- gsub(x = names(eb_data), pattern = "X", replacement = "Use_")  
+eb_data$informant<-sample(c('User_1', 'User_2', 'User_3'), 20, replace=TRUE)
+eb_data$sp_name<-sample(c('sp_1', 'sp_2', 'sp_3', 'sp_4'), 20, replace=TRUE)
+ethnoChord(eb_data)
+
+#Make function blue####
+#copy and paste after calling 'ethnoChord' in the Console
+New_Chord_function_blue<- function(data) { #use the '<-' to assign function to new object
+  if (!requireNamespace("reshape", quietly = TRUE)) {
+    stop("Package \"reshape\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+  if (!requireNamespace("circlize", quietly = TRUE)) {
+    stop("Package \"circlize\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+  if (!requireNamespace("dplyr", quietly = TRUE)) {
+    stop("Package \"dplyr\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+  if (!requireNamespace("magrittr", quietly = TRUE)) {
+    stop("Package \"magrittr\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+  
+  sp_name <- informant <- value <- strwidth <- NULL # Setting the variables to NULL first, appeasing R CMD check
+  
+  #add error stops with validate_that
+  assertthat::validate_that("informant" %in% colnames(data), msg = "The required column called \"informant\" is missing from your data. Add it.")
+  assertthat::validate_that("sp_name" %in% colnames(data), msg = "The required column called \"sp_name\" is missing from your data. Add it.")
+  
+  assertthat::validate_that(is.factor(data$informant), msg = "The \"informant\" is not a factor variable. Transform it.")
+  assertthat::validate_that(is.factor(data$sp_name), msg = "The \"sp_name\" is not a factor variable. Transform it.")
+  
+  assertthat::validate_that(all(sum(dplyr::select(data, -informant, -sp_name)>0)) , msg = "The sum of all UR is not greater than zero. Perhaps not all uses have values or are not numeric.")
+  
+  ## Use 'complete.cases' from stats to get to the collection of obs without NA
+  data_complete<-data[stats::complete.cases(data), ]
+  #message about complete cases
+  assertthat::see_if(length(data_complete) == length(data), msg = "Some of your observations included \"NA\" and were removed. Consider using \"0\" instead.")
+  
+  #Melt ethnobotany data
+  mat <- reshape::melt(data, id=c("informant","sp_name")) %>% dplyr::filter(value >=1)%>%
+    dplyr::arrange(dplyr::desc(informant)) %>%  dplyr::arrange(dplyr::desc(sp_name)) %>% dplyr::select(2:3)
+  
+  #Create chord plot
+  
+  circlize::chordDiagram(mat, annotationTrack = "grid", 
+                         preAllocateTracks = list(track.height = max(graphics::strwidth(unlist(dimnames(mat))))))
+  circlize::circos.track(track.index = 1, panel.fun = function(x, y) {
+    xlim = circlize::get.cell.meta.data("xlim")
+    xplot = circlize::get.cell.meta.data("xplot")
+    ylim = circlize::get.cell.meta.data("ylim")
+    sector.name = circlize::get.cell.meta.data("sector.index")
+    
+    if(abs(xplot[2] - xplot[1]) < 20) {
+      circlize::circos.text(mean(xlim), ylim[1], sector.name, facing = "clockwise",
+                            niceFacing = TRUE, adj = c(0, 0.5), col = "blue")
+    } else {
+      circlize::circos.text(mean(xlim), ylim[1], sector.name, facing = "clockwise",
+                            niceFacing = TRUE, adj = c(0, 0.5), col = "blue")
+    }
+  }, bg.border = NA)
+  
+  print("Chord diagram for each use related to each species in the data set")
+}
+
+#Running new blue function
+New_Chord_function_blue(ethnobotanydata)
+
+#Running all functions, original, red and blue####
+ethnoChord(ethnobotanydata)
+New_Chord_function_red(ethnobotanydata)
+New_Chord_function_blue(ethnobotanydata)
+
+#Formulas and R script for assignments ####
+
+a<-1+1
+b<-3*7
+a+b
+
+b<-"fred"
+b
+
+#Use functions for calculations####
+
+#Function to calculate URs ####
+
+new_URs_function <- function(data) {
+  if (!requireNamespace("dplyr", quietly = TRUE)) {
+    stop("Package \"dplyr\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+  if (!requireNamespace("magrittr", quietly = TRUE)) {
+    stop("Package \"magrittr\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+  
+  URdata <- URs <- sp_name <- informant <- URps <- NULL # Setting the variables to NULL first, appeasing R CMD check
+  
+  #add error stops with validate_that
+  assertthat::validate_that("informant" %in% colnames(data), msg = "The required column called \"informant\" is missing from your data. Add it.")
+  assertthat::validate_that("sp_name" %in% colnames(data), msg = "The required column called \"sp_name\" is missing from your data. Add it.")
+  
+  assertthat::validate_that(is.factor(data$informant), msg = "The \"informant\" is not a factor variable. Transform it.")
+  assertthat::validate_that(is.factor(data$sp_name), msg = "The \"sp_name\" is not a factor variable. Transform it.")
+  
+  assertthat::validate_that(all(sum(dplyr::select(data, -informant, -sp_name)>0)) , msg = "The sum of all UR is not greater than zero. Perhaps not all uses have values or are they are not numeric.")
+  
+  ## Use 'complete.cases' from stats to get to the collection of obs without NA
+  data_complete<-data[stats::complete.cases(data), ]
+  #message about complete cases
+  assertthat::see_if(length(data_complete) == length(data), msg = "Some of your observations included \"NA\" and were removed. Consider using \"0\" instead.")
+  
+  URdata <- data #create subset-able data
+  
+  URdata$URps <- dplyr::select(URdata, -informant, -sp_name) %>% rowSums()
+  URs <- URdata %>% dplyr::group_by(sp_name) %>%
+    dplyr::summarize (URs = mean(URps))%>% #change from 'sum' to 'mean'
+    dplyr::arrange(-URs) 
+  
+  print(as.data.frame(URs))
+}
+
+#Run new URs formula (wrong with 'mean' instead of 'sum')
+new_URs_function(ethnobotanydata)
 
